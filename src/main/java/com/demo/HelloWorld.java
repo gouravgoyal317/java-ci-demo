3a24cc87ceeadd931723e6795d3d40d6dedd80b2
gouravgoyal317 <sourabgoyal82@gmail.com>
2026-06-12 20:25:17 +0530

    Java-ci-demo

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
+@SpringBootApplication
+public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        SpringApplication.run(Application.class,args);
     }
 }
